b2f18d60003b46b4d26d871c74d7aaa90c922e4a
CJINBNH <alex_egorov_89@mail.ru>
2022-08-22 21:36:44 +0400

    Completed task_001

--- task_001/Program.cs
+++ task_001/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая перевернет одномерный массив
+
+const int ARRAY_SIZE =  5;
+const int LEFT_RANGE =  0;
+const int RIGHT_RANGE =  9;
+
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    Random random = new Random();
+    int[] newArray = new int[size];
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = random.Next(leftRange, rightRange + 1);
+    }
+    return newArray;
+}
+ void ReverseArray(int[] newArray)
+ {
+    int temp = 0;
+    for (int i = 0; i < newArray.Length/2; i++)
+    {
+        temp = newArray[i];
+        newArray[i] = newArray[newArray.Length - 1 - i];
+        newArray[newArray.Length - 1 - i] = temp; 
+    }
+ }
+
+ int[] array = FillArray(ARRAY_SIZE, LEFT_RANGE, RIGHT_RANGE);
+ Console.WriteLine('[' + string.Join(", ", array) + ']');
+ ReverseArray(array);
+ Console.WriteLine('[' + string.Join(", ", array) + ']');
