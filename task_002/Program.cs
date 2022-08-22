6cc450c5ab0f94e56141f5bbe36bd4d00c530277
CJINBNH <alex_egorov_89@mail.ru>
2022-08-22 21:39:52 +0400

    Completed task_002

--- task_002/Program.cs
+++ task_002/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины
+// Необходимые условия существования треугольника a+b>c, a+c>b, b+c>a
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int third = Convert.ToInt32(Console.ReadLine());
+
+if (a + b > third && a + third > b && b + third > a)
+{
+    Console.WriteLine("Треугольник со сторонами a, b и c существовать может");
+}
+else Console.WriteLine("Треугольник существовать не может");
